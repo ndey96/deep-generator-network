24a8edc3ab8642477c12634ea89a9e377d0497c2
kothiga <kothiga2112@gmail.com>
2020-04-06 22:12:09 -0400

    Completed Training and Valid loop. Wrote some TODOs for Nick ;).

--- train.py
+++ train.py
@@ -22,20 +22,14 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
         shutil.copyfile(filename, 'model_best.pth.tar')
 
 
-def generator_loss(a, a_hat, x, x_hat, comparator, discriminator, lambda_feat=0.01, lambda_adv=0.001, lambda_img=1.0):
+def generator_loss(a, x, x_hat, comparator, discriminator, lambda_feat=0.01, lambda_adv=0.001, lambda_img=1.0):
     '''
         Input:
          a      enc ( input )
-         a_hat  enc ( x_hat )
          x      input
          x_hat  gen ( enc (input) ) 
     '''
 
-    # deep_feature_loss = torch.sum((comparator(x) - comparator(x_hat)) ** 2)
-    # reconstruction_loss = torch.sum((x - x_hat) ** 2)
-    # discriminator_loss = discriminator(69) 
-    # loss = discriminator_loss + 0.1 * deep_feature_loss + 0.001 * reconstruction_loss
-    
     # Loss in feature space.
     loss_feat = torch.sum( (comparator(x_hat) - comparator(x))**2 )
 
@@ -43,33 +37,37 @@ def generator_loss(a, a_hat, x, x_hat, comparator, discriminator, lambda_feat=0.
     loss_img = torch.sum( (x_hat - x)**2 )
 
     # Adversarial losses.
-    gen_discr  = discriminator(x_hat, a)
     real_discr = discriminator(x, a)
-
+    gen_discr  = discriminator(x_hat, a)
+    
     loss_discr = -1.0 * torch.sum( torch.log(real_discr) + torch.log(1.0 - gen_discr) )
     loss_adv   = -1.0 * torch.sum( torch.log(gen_discr) )
     
     # Combine the losses for DeePSiM.
     loss = lambda_feat * loss_feat + lambda_adv * loss_adv + lambda_img * loss_img
 
-    return loss, loss_discr
+    return loss, loss_discr, (real_discr, gen_discr)
 
 
 
-def train(loader, optimizer, generator, discriminator, encoder, comparator, train_generator, train_discrimin):
+def train(loader, optim_gen, generator, optim_discr, discriminator, encoder, comparator, train_generator, train_discrimin, device, verbose):
     
-    #generator.train() TODO: REMOVE.
-    #num_batches = 0
-    loss_sum = 0
+    # Put in training mode.
+    generator.train()
+    discriminator.train()
 
-    for i, (inp, tar) in enumerate(loader):
+    # Set up some counters.
+    gen_loss_sum   = 0.0
+    discr_loss_sum = 0.0
+    num_batches    = 0
 
-        # target = target.cuda(async=True) # TODO: ???
+    for i, (inp, _) in enumerate(loader):
+
+        # target = target.cuda(async=True) # TODO [NICK]: Look into this for using your 30,000 GPUs.
         
+        # Prime the input.
         input_var  = torch.autograd.Variable(inp)
-        target_var = torch.autograd.Variable(tar)
-
-        # TODO: Set input_var and target_var to take in the device (cpu/cuda).
+        input_var = input_var.to(device)
 
 
         #
@@ -84,33 +82,22 @@ def train(loader, optimizer, generator, discriminator, encoder, comparator, trai
         #
         # ( x_hat )   =   gen ( a )
         generator_out = generator(features_real)
-
-        # TODO: Should the generator take input_var or features_out??
         
 
+        #
+        # TODO: REMOVE.
         #
         # 3) Encode the generated image for comparison of features.
         #
         # ( a_hat )    =    enc ( x_hat )
-        features_recog = encoder(generator_out)
+        #features_recog = encoder(generator_out)
 
-        
-        #
-        # TODO: Is this necessary?? Should Feeding of discriminator happen out here?? 
-        #       If so, how should it be done??
-        #
-        # 4) Run the discriminator on the real features to get the ___ (loss?)
-        #
-        # discrim_real = discriminator(input_var,     features_real)
-        # discrim_fake = discriminator(generator_out, features_real)
-        
 
         #
         # 4) Compute the loss of the generator.
         #
-        gen_loss, discr_loss = generator_loss(
+        gen_loss, discr_loss, (real_discr, gen_discr) = generator_loss(
             a=features_real, 
-            a_hat=features_recog, 
             x=input_var, 
             x_hat=generator_out, 
             comparator=comparator, 
@@ -119,67 +106,125 @@ def train(loader, optimizer, generator, discriminator, encoder, comparator, trai
 
 
         #
-        # TODO: This is one possible way of turning on/off the weights for each network 
+        # 5) Compute the gradient and take a step.
         #
-        # 5) Enable/Disable weights depending on out of function switches.
-        #
-        # If train_x = True  : Parameters will be effected by grad-update.
-        # If train_x = False : Parameters will be uneffected by grad-update.
-        for gen_param in generator.parameters():
-            gen_param.requires_grad = train_generator
-        for dis_param in discriminator.parameters():
-            dis_param.requires_grad = train_discrimin
+        if train_generator:
+            optim_gen.zero_grad()
+            gen_loss.backward()
+            optim_gen.step()
+
+        if train_discrimin:
+            optim_discr.zero_grad()
+            discr_loss.backward()
+            optim_discr.step()
 
 
         #
-        # 6) Compute the Gradient and do a SGD step.
+        # 6) Update counters.
         #
-        optimizer.zero_grad()
-        gen_loss.backward()
-        optimizer.step()
-
-
-    #
-    # TODO: Figure out what to return here.
-    #
-
-    avg_loss = loss_sum
-    # print(f'Avg loss: {avg_loss}')
-    #return avg_loss # ???
-    
-    discr_real_loss = 0.0
-    discr_fake_loss = 0.0
-    discr_fake_for_generator_loss = 1.0
-    #return discr_real_loss, discr_fake_loss, discr_fake_for_generator_loss # ???
-    
-    discr_loss_ratio = (discr_real_loss + discr_fake_loss) / discr_fake_for_generator_loss
-    return discr_loss_ratio # ???
-
-
-
-# TODO: Work out validate function.
-def validate(loader, generator, encoder, comparator):
-    generator.eval()
-    num_batches = 0
-    loss_sum = 0
-    for i, (input, _) in enumerate(loader):
-        x = torch.autograd.Variable(input)
-        x = x.to(device)
-
-        # compute activation for image
-        a = encoder(x)
-        # breakpoint()
-        # compute output
-        x_hat = generator(a)
-
-        loss = generator_loss(a, a_hat, x, x_hat, comparator, discriminator)
-        loss_sum += loss
+        gen_loss_sum += gen_loss
+        discr_loss_sum += discr_loss
         num_batches += 1
 
-    avg_loss = loss_sum / num_batches
-    # print(f'Avg loss: {avg_loss}')
 
-    return avg_loss
+        if verbose:
+            print('[TRAIN] {:3.0f} : Gen_Loss={:0.5} -- Dis_Loss={:0.5}'.format(num_batches, gen_loss, discr_loss))
+
+        #
+        # 7) Switch optimizing discriminator and generator, so that neither of them overfits too much.
+        #
+        discr_loss_ratio = ( real_discr + gen_discr ) / discr_loss
+
+        if discr_loss_ratio < 1e-1 and train_discrimin:
+            train_discrimin = False
+            train_generator = True
+
+        if discr_loss_ratio > 5e-1 and not train_discrimin:
+            train_discrimin = True
+            train_generator = True
+
+        if discr_loss_ratio > 1e1 and train_generator:
+            train_generator = False
+            train_discrimin = True
+
+
+    gen_loss_sum /= num_batches
+    discr_loss_sum /= num_batches
+
+    return gen_loss_sum, discr_loss_sum, train_generator, train_discrimin
+
+
+
+def validate(loader, generator, discriminator, encoder, comparator, device, verbose):
+
+    # Put in evaluation mode.
+    generator.eval()
+    discriminator.eval()
+
+    gen_loss_sum   = 0.0
+    discr_loss_sum = 0.0
+    num_batches    = 0
+
+    for i, (inp, _) in enumerate(loader):
+        
+        # target = target.cuda(async=True) # TODO [NICK]: Here too.
+
+        # Prime the input.
+        input_var  = torch.autograd.Variable(inp)
+        input_var = input_var.to(device)
+
+
+        #
+        # 1) Feed forward the data into the encoder.
+        #
+        #    ( a )    =    enc ( x )
+        features_real = encoder(input_var)
+
+
+        #
+        # 2) Feed forward the data into the generator.
+        #
+        # ( x_hat )   =   gen ( a )
+        generator_out = generator(features_real)
+        
+
+        #
+        # TODO: REMOVE.
+        #
+        # 3) Encode the generated image for comparison of features.
+        #
+        # ( a_hat )    =    enc ( x_hat )
+        #features_recog = encoder(generator_out)
+
+
+        #
+        # 4) Compute the loss of the generator.
+        #
+        gen_loss, discr_loss, _ = generator_loss(
+            a=features_real, 
+            x=input_var, 
+            x_hat=generator_out, 
+            comparator=comparator, 
+            discriminator=discriminator
+        )
+
+
+        #
+        # 6) Update counters.
+        #
+        gen_loss_sum += gen_loss
+        discr_loss_sum += discr_loss
+        num_batches += 1
+
+        if verbose:
+            print('[VALID] {:3.0f} : Gen_Loss={:0.5} -- Dis_Loss={:0.5}'.format(num_batches, gen_loss, discr_loss))
+
+
+    gen_loss_sum /= num_batches
+    discr_loss_sum /= num_batches
+
+    return gen_loss_sum, discr_loss_sum
+
 
 
 
@@ -212,6 +257,8 @@ if __name__ == '__main__':
         num_workers=8,
         pin_memory=True)
 
+
+    # Init the models.
     encoder = AlexNetEncoder() #TODO: pass in ``device`` as arg?
     encoder.cuda()
     encoder.eval()
@@ -226,61 +273,76 @@ if __name__ == '__main__':
     discriminator = Discriminator()
     discriminator.cuda()
 
-    optimizer = torch.optim.SGD(
+
+    # Set up the optimizers.
+    optim_gen = torch.optim.SGD(
         generator.parameters(),
         lr=0.1,
         momentum=0.9,
         weight_decay=1e-4
     )
 
+    optim_discr = torch.optim.adam(
+        discriminator.parameters(),
+        lr=0.0002,
+        momentum=0.9,
+        weight_decay=1e-4
+    )
+
+
+    # Begin training.
     print('Beginning training...')
 
-    avg_train_losses = []
-    avg_valid_losses = []
+    avg_train_gen_losses   = []
+    avg_train_discr_losses = []
+    
+    avg_valid_gen_losses   = []
+    avg_valid_discr_losses = []
 
-    train_discrimin = True
+
     train_generator = True
+    train_discrimin = True
 
+    verbose=True
     for epoch in range(100):
     
-        avg_train_loss = train(
+        train_gen_loss, train_discr_loss, train_generator, train_discrimin = train(
             loader=train_loader, 
-            optimizer=optimizer, 
+            optim_gen=optim_gen, 
             generator=generator,
+            optim_discr=optim_discr,
             discriminator=discriminator,
             encoder=encoder, 
             comparator=comparator,
             train_generator=train_generator,
-            train_discrimin=train_discrimin
+            train_discrimin=train_discrimin,
+            device=device,
+            verbose=verbose
         )
         
-        avg_valid_loss = validate(
+
+        valid_gen_loss, valid_discr_loss = validate(
             loader=val_loader, 
             generator=generator, 
+            discriminator=discriminator,
             encoder=encoder, 
-            comparator=comparator
+            comparator=comparator,
+            device=device,
+            verbose=verbose
         )
         
-        avg_train_losses.append( avg_train_loss )
-        avg_valid_losses.append( avg_valid_loss )
 
+        #TODO [NICK]: Set up torchvision/tensorboard to visualize these lists??
+        avg_train_gen_losses.append(train_gen_loss)
+        avg_train_discr_losses.append(train_discr_loss)
+
+        avg_valid_gen_losses.append(valid_gen_loss)
+        avg_valid_discr_losses.append(valid_discr_loss)
         
-        # Switch optimizing discriminator and generator, so that neither of them overfits too much
-        if avg_train_loss < 1e-1 and train_discrimin:
-            train_discrimin = False
-            train_generator = True
 
-        if avg_train_loss > 5e-1 and not train_discrimin:
-            train_discrimin = True
-            train_generator = True
-
-        if avg_train_loss > 1e1 and train_generator:
-            train_generator = False
-            train_discrimin = True
-
-        print(f'Epoch {epoch}: avg_train_loss={avg_train_loss:.3f}, avg_valid_loss={avg_valid_loss:.3f}')
-
+        #TODO [NICK]: Set up saving and loading of the gen and discr weights.
         save_checkpoint({
-            'epoch': epoch + 1,
-            'state_dict': generator.state_dict(),
+            'epoch'            : epoch + 1,
+            'state_dict_gen'   : generator.state_dict(),
+            'state_dict_discr' : discriminator.state_dict(),
         }, is_best=False)
