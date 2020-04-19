d7fe219831ee51be4ad2803227879cf273a924ea
kothiga <kothiga2112@gmail.com>
2020-04-18 23:35:10 -0400

    Work in progress Synth script.

--- synthesize.py
+++ synthesize.py
@@ -5,6 +5,7 @@ import time
 
 import torch
 import torch.nn as nn
+from torchvision.models import alexnet
 
 from models_parallel import DeepSim
 from models_parallel import DeepGen
@@ -12,25 +13,22 @@ from models_parallel import DeepGen
 from checkpoint_stub import load_checkpoint
 from optimizer_stub  import get_optimizers
 
-
-# TODO: optimize input to generator that will maximize the 
-#        activation of a particular output neuron in AlexNet
-
 import argparse
+
 
 def get_args():
     
     # Init a parser.
     parser = argparse.ArgumentParser (
         prog='Synthesize', 
-        description='Provided a class number in Range=[0, ...], synthesize an image through the Deep Sim model, or our ``improved`` Deep Generator model (or both!!)',
+        description='Provided a class number in Range=[0, 1000], synthesize an image through the Deep Sim model, or our ``improved`` Deep Generator model (or both!!)',
         usage='python synthesize.py [--class CLASS] [--sim] [--gen] [--save] [--cuda] [--verb]'
     )
     
     # Add arguments to parser.
     #parser.add_argument('--img',  default=None,  help='Path to an image to pass through the selected architecture(s)                 (default: {})'.format('[REQUIRED]')) #TODO: REMOVE. But maybe keep for a style transfer demo...?
     
-    parser.add_argument('--class', dest='_class', type=int, default=0, help='Synthesize the highest activation image of this class. Range=[0, ...] (default: {})'.format(0)) #TODO: Find which class is ~ C O R N ~
+    parser.add_argument('--class', dest='_class', type=int, default=0, help='Synthesize the highest activation image of this class. Range=[0, 1000] (default: {})'.format(0)) #TODO: Find which class is ~ C O R N ~
     parser.add_argument('--sim',   action='store_true', default=False, help='Pass the provided image through the DeepSim architecture              (default: {})'.format('DISABLED'))
     parser.add_argument('--gen',   action='store_true', default=False, help='Pass the provided image through the DeepGen architecture              (default: {})'.format('DISABLED'))
     parser.add_argument('--show',  action='store_true', default=False, help='Show the initial and generated images                                 (default: {})'.format('DISABLED'))
@@ -49,27 +47,42 @@ def get_args():
     return args
 
 
+# TODO: Make a function to save a stack of images as a .gif
+
+
 def show_image(img, name="", save=False, verbose=False):
 
     plt.figure()
     plt.imshow(img, aspect='equal')
+    plt.tight_layout()
     plt.axis('off')
     plt.title(name)
 
     if save:
-        plt.imsave(save+'.png', img, format='png')
-        if verbose: print('[INFO] Saved ``{}``'.format(name))
+        plt.imsave('./img/{}.png'.format(name), img, format='png')
+        if verbose: print('[INFO] Saved ``{}.png``'.format(name))
 
     return
 
 
-def synthesize(model, neuron=0):
+def synthesize(model, classifier, neuron=0, device=torch.device("cpu")):
 
+    # Make sure we're in evaluation mode.
     model.eval()
+    classifier.eval()
 
-    print(model.get_device())
-
-    return np.random.rand(128, 128, 3)
+    code = torch.rand( 1, 4096 ).to(device) # Optimize for this.
+    x = model.G(code) # Normalize??
+    y = classifier(x)
+    
+    # TODO: Revisit.
+    out_img = x.cpu().detach().numpy().reshape(227, 227, 3, order='F')
+    out_cls = y.cpu().detach().numpy()
+    
+    # TODO: Might need to normalize the image!!?
+    out_img += np.abs(out_img.min())
+    
+    return out_img, out_cls
 
 
 def main():
@@ -82,8 +95,13 @@ def main():
 
     # Init a cuda device if requested and available.
     if args.cuda and not torch.cuda.is_available():
-        print('[ERROR] Requested is of CUDA when unavailable... Continuing with cpu.')
+        print('[ERROR] Requested use of CUDA when unavailable... Continuing with CPU.')
     device = torch.device("cuda:0" if (args.cuda and torch.cuda.is_available()) else "cpu")
+
+    
+    # Init the imagenet classifier AlexNet.
+    AlexNet = alexnet(pretrained=True)
+    AlexNet.to(device)
 
 
     # Process with the DeepSim model.
@@ -93,22 +111,26 @@ def main():
         model = DeepSim()
         model.to(device)
 
-        opt_g, opt_d = get_optimizers(model, lr=0.0002)
-        model, *_ = load_checkpoint(model, opt_g, opt_d, filename='./models/17_04_2020-18-40-21_10009_128_lf0.01_la1_li0.01_lr0.0002.ptm')
+        #
+        #TODO: need consistency between models_parallel, optimizer_stub, & checkpoint_stub.
+        #
+        #opt_g, opt_d = get_optimizers(model, lr=0.0002)
+        #model, *_ = load_checkpoint(model, opt_g, opt_d, filename='./chk/17_04_2020-18-40-21_10009_128_lf0.01_la1_li0.01_lr0.0002.ptm')
 
+        #TODO: model go cuda here?
+        #model.to(device)
 
         # Begin processing.
         begin = time.time()
-        sim_image = synthesize(model, neuron=args._class)
+        sim_image, alex_class = synthesize(model, AlexNet, neuron=args._class, device=device)
         end = time.time()
-
 
         # Let me know when things have finished processing.
         if args.verb:
             print('[INFO] Completed processing SIM in {:0.4}(s)'.format(end - begin)) 
 
         # Save/Show the image.
-        show_image(img=sim_image, name=now+' - Sim_{:03d}'.format(args._class), save=args.save, verbose=args.verb) #TODO: Depending on the number of classes make that 3 a 4 or even 5!!
+        show_image(img=sim_image, name='{} - Sim_{:04d}'.format(now, args._class), save=args.save, verbose=args.verb)
 
 
     #TODO: Copy above for DEEPGEN.
