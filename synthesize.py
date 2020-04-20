0376ca1e8a1b04aa46611c72ca5a3cf4a8425c2f
kothiga <kothiga2112@gmail.com>
2020-04-20 13:46:39 -0400

    Cleaned up Synth. Now with .gifsgit pull

--- synthesize.py
+++ synthesize.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import image
+from matplotlib.animation import ArtistAnimation
 import time
 
 import torch
@@ -28,11 +29,14 @@ def get_args():
     # Add arguments to parser.
     #parser.add_argument('--img',  default=None,  help='Path to an image to pass through the selected architecture(s)                 (default: {})'.format('[REQUIRED]')) #TODO: REMOVE. But maybe keep for a style transfer demo...?
     
-    parser.add_argument('--class', dest='_class', type=int, default=0, help='Synthesize the highest activation image of this class. Range=[0, 1000] (default: {})'.format(0)) #TODO: Find which class is ~ C O R N ~
+    parser.add_argument('--class', type=int, default=0, dest='_class', help='Synthesize the highest activation image of this class. Range=[0, 1000] (default: {})'.format(0)) #TODO: Find which class is ~ C O R N ~
+    parser.add_argument('--steps', type=int, default=100,              help='The number of steps that should be taken to find the code.            (default: {})'.format(100))
     parser.add_argument('--sim',   action='store_true', default=False, help='Pass the provided image through the DeepSim architecture              (default: {})'.format('DISABLED'))
     parser.add_argument('--gen',   action='store_true', default=False, help='Pass the provided image through the DeepGen architecture              (default: {})'.format('DISABLED'))
     parser.add_argument('--show',  action='store_true', default=False, help='Show the initial and generated images                                 (default: {})'.format('DISABLED'))
     parser.add_argument('--save',  action='store_true', default=False, help='Save the output generated image                                       (default: {})'.format('DISABLED'))
+    parser.add_argument('--vid',   action='store_true', default=False, help='Save the morphing images as a .mp4                                    (default: {})'.format('DISABLED'))
+    parser.add_argument('--freq',  type=int, default=1,                help='Frequency at which images should be saved for .mp4                    (default: {})'.format(1))
     parser.add_argument('--cuda',  action='store_true', default=False, help='Enable CUDA for processing                                            (default: {})'.format('DISABLED'))
     parser.add_argument('--verb',  action='store_true', default=False, help='Enable Verbose output                                                 (default: {})'.format('DISABLED'))
     args = parser.parse_args()
@@ -47,7 +51,38 @@ def get_args():
     return args
 
 
-# TODO: Make a function to save a stack of images as a .gif
+
+def convert(img, target_type_min, target_type_max, target_type):
+    imin = img.min()
+    imax = img.max()
+
+    a = (target_type_max - target_type_min) / (imax - imin)
+    b = target_type_max - a * imax
+    new_img = (a * img + b).astype(target_type)
+    return new_img
+
+
+
+def show_vid(img, name="", save=False, verbose=False):
+
+    fig = plt.figure()
+    plt.tight_layout()
+    plt.axis('off')
+    plt.title(name)
+    ima = []
+    for cur in img:
+        im = plt.imshow(cur, animated=True, aspect='equal')
+        ima.append( [im] )
+    ani = ArtistAnimation(fig, ima, interval=30, blit=True)
+    
+    if save:
+        import imageio
+        img_gif = convert(np.asarray(img), 0, 255, np.uint8)
+        imageio.mimwrite('img/{}.gif'.format(name), img_gif, fps=32)
+        if verbose: print('[INFO] Saved ``{}.fig``'.format(name))
+        
+    return
+    
 
 
 def show_image(img, name="", save=False, verbose=False):
@@ -65,8 +100,8 @@ def show_image(img, name="", save=False, verbose=False):
     return
 
 
-def synthesize(model, classifier, neuron=0, num_steps=10, lr=0.005, wdecay = 0.001, num_class=1000, code_len=4096, device=torch.device("cpu"), keep_steps=False, keep_freq=1, verbose=True):
-    #num_steps=300
+
+def synthesize(model, classifier, neuron=0, num_steps=300, lr=0.005, wdecay=0.0001, num_class=1000, code_len=4096, dims=(227, 227, 3), device=torch.device("cpu"), keep_steps=False, keep_freq=1, verbose=True):
     
     # Init a random code to start from.
     code = torch.rand( 1, code_len ).to(device) 
@@ -78,8 +113,11 @@ def synthesize(model, classifier, neuron=0, num_steps=10, lr=0.005, wdecay = 0.0
     classifier.eval()
 
     step = 0
+    meta = 0
+    keep_imgs = []
     while step < num_steps:
         step += 1
+        meta = 0
 
         def closure():
             
@@ -95,7 +133,11 @@ def synthesize(model, classifier, neuron=0, num_steps=10, lr=0.005, wdecay = 0.0
             out = classifier(y)
 
             #TODO: REVIST CLAMP.
-            #out = torch.clamp(out, min=0.0, max=3.0*torch.std(out[0]).item())
+            #out = torch.clamp(
+            #    out, 
+            #    min = -3.0*torch.std(out[0]).item(), 
+            #    max =  3.0*torch.std(out[0]).item()
+            #)
             
             # Get the loss with L2 weight decay.
             loss = -out[0, neuron] + wdecay * torch.sum( code**2 )
@@ -103,14 +145,21 @@ def synthesize(model, classifier, neuron=0, num_steps=10, lr=0.005, wdecay = 0.0
             loss.backward()
             
             if verbose:
-                print(step, ":", "{} ?= {}".format(torch.argmax(out), neuron), 
-                    "\nloss={}".format(loss.data), 
-                    "\nclass={}".format(out[0,:5].data), 
-                    "\ncode={}".format(code[0,:5].data))
-        
+                print("[INFO] {:03d} : {} ?= {}".format(step, torch.argmax(out), neuron), 
+                    "\n   loss  = {}".format(loss.data), 
+                    "\n   class = {}".format(out[0,:5].data), 
+                    "\n   code  = {}".format(code[0,:5].data))
+
             return loss
         
         optimizer.step(closure)
+
+        if keep_steps and ((step % keep_freq) == 0):
+            
+            y = model.module.G(code)
+            y = (y + 1.0 ) / 2.0
+            
+            keep_imgs.append(y.cpu().detach().numpy().T.reshape(*dims))
 
 
     # Get the final image & guessed class label.
@@ -119,13 +168,14 @@ def synthesize(model, classifier, neuron=0, num_steps=10, lr=0.005, wdecay = 0.0
 
     out = classifier(y)
     
-    out_img = y.cpu().detach().numpy().T.reshape(227, 227, 3)
+    out_img = y.cpu().detach().numpy().T.reshape(*dims)
     out_cls = torch.argmax(out).cpu().detach().numpy()
     
     #TODO: REMOVE.
     print(out_img.max(), out_img.min())
     
-    return out_img, out_cls
+    return out_img, out_cls, keep_imgs
+
 
 
 def main():
@@ -155,22 +205,38 @@ def main():
         model.to(device)
 
         opt_g, opt_d = get_optimizers(model, lr=0.0002)
-        model, *_ = load_checkpoint(model, opt_g, opt_d, filename='./chk/dg18_04_2020-22-47-34_30027_128_lf1_la0.0625_li3_lr0.0001.ptm')
+        model, *_ = load_checkpoint(model, opt_g, opt_d, filename='./chk/ds19_04_2020-22-39-16_120108_64_lf1_la0.0625_li3_lr0.0002.ptm')
 
         # Begin processing.
         begin = time.time()
-        sim_image, alex_class = synthesize(model, AlexNet, neuron=args._class, device=device, verbose=args.verb)
+    
+        sim_image, alex_class, sim_video = synthesize(
+            model=model, 
+            classifier=AlexNet, 
+            neuron=args._class, 
+            num_steps=args.steps,
+            lr=0.005, 
+            wdecay=0.0001,
+            device=device, 
+            keep_steps=args.vid, 
+            keep_freq=args.freq,
+            verbose=args.verb
+        )
         end = time.time()
 
         # Let me know when things have finished processing.
         if args.verb:
-            print('[INFO] Completed processing SIM in {:0.4}(s)'.format(end - begin)) 
+            print('[INFO] Completed processing SIM in {:0.4}(s)!! Requested Class {} -- Generated Class {}'.format(end - begin, args._class, alex_class)) 
 
         # Save/Show the image.
         show_image(img=sim_image, name='{} - Sim_{:04d}'.format(now, args._class), save=args.save, verbose=args.verb)
 
+        if args.vid:
+            show_vid(img=sim_video, name='{} - Sim_{:04d}'.format(now, args._class), save=args.save, verbose=args.verb)
+
 
     #TODO: Copy above for DEEPGEN.
+
 
     if args.show:
         # Show the images at the very end.
